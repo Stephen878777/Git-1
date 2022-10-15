094935fdb90d20ebbd5db3225fd036f315cf8776
Esteban pino <estrami987@gmail.com>
2022-10-14 23:26:21 -0500

    agregamos una function

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hola me falto este mensaje');
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  function showOk() {
+    alert( "You agree." );
+  }
+  
+  function showCancel() {
+    alert( "You cancelled execution." );
+  }
+  
+  
+  ask("you agree?", showOk, showCancel);
